ad0c55cb56c46291a3b5a720b39ae7408b98d4a1
dylanjcassidy <dylan.j.cassidy@uconn.edu>
2026-01-31 18:18:38 -0500

    working

--- Scenes/sphere_car.gd
+++ Scenes/sphere_car.gd
@@ -1,38 +1,50 @@
-extends Node3D
+extends RigidBody3D
 
-@onready var m_ball = %ball
-@onready var m_mesh = %mesh
-@onready var m_ground_ray = %mesh/raycast
+var sphere_offset = Vector3.DOWN
+var acceleration = 35.0
+var steering = 19.0
+var turn_speed = 4.0
+var turn_stop_limit = 0.75
+var body_tilt = 35
 
-var m_sphere_offset = Vector3(0, 1.0, 0)
-@export var m_acceleration = 50.0
-@export var m_steering = 21.0
-@export var m_turn_speed = 5.0
-@export var m_turn_stop_limit = 0.75
+var speed_input = 0
+var turn_input = 0
 
-var m_speed_input = 0
-var m_rotate_input = 0
+@onready var car_mesh = $mesh
+#@onready var body_mesh = $CarMesh/suv2
+@onready var ground_ray = $mesh/raycast
+#@onready var right_wheel = $CarMesh/suv2/wheel_frontRight
+#@onready var left_wheel = $CarMesh/suv2/wheel_frontLeft
 
-func _ready() -> void:
-    m_ground_ray.add_exception(m_ball)
+func _ready():
+	ground_ray.add_exception(self)
+	
+func _physics_process(delta):
+	car_mesh.position = position + sphere_offset
+	if ground_ray.is_colliding():
+		apply_central_force(-car_mesh.global_transform.basis.z * speed_input)
+	
+func _process(delta):
+	if not ground_ray.is_colliding():
+		return
+	speed_input = Input.get_axis("decelerate", "accelerate") * acceleration
+	turn_input = Input.get_axis("turn_right", "turn_left") * deg_to_rad(steering)
+	#right_wheel.rotation.y = turn_input
+	#left_wheel.rotation.y = turn_input
+	
+	if linear_velocity.length() > turn_stop_limit:
+		var new_basis = car_mesh.global_transform.basis.rotated(car_mesh.global_transform.basis.y.normalized(), turn_input)
+		car_mesh.global_transform.basis = car_mesh.global_transform.basis.slerp(new_basis, turn_speed * delta)
+		car_mesh.global_transform = car_mesh.global_transform.orthonormalized()
+		var t = -turn_input * linear_velocity.length() / body_tilt
+		#body_mesh.rotation.z = lerp(body_mesh.rotation.z, t, 5.0 * delta)
+		if ground_ray.is_colliding():
+			var n = ground_ray.get_collision_normal()
+			var xform = align_with_y(car_mesh.global_transform, n)
+			car_mesh.global_transform = car_mesh.global_transform.interpolate_with(xform, 10.0 * delta)
 
-func _physics_process(_delta):
-    # Keep the car mesh aligned with the sphere
-    m_mesh.transform.origin = m_ball.transform.origin - m_sphere_offset
-    # Accelerate based on car's forward direction
-    m_ball.apply_central_force(-m_mesh.global_transform.basis.z * m_speed_input)
-
-func _process(delta: float) -> void:
-    if not m_ground_ray.is_colliding():
-        return
-    else:
-        # Get accelerate/brake input
-        m_speed_input = 0
-        m_speed_input += Input.get_action_strength("accelerate")
-        m_speed_input -= Input.get_action_strength("decelerate")
-        m_speed_input *= m_acceleration
-        # Get steering input
-        m_rotate_input = 0
-        m_rotate_input += Input.get_action_strength("turn_left")
-        m_rotate_input -= Input.get_action_strength("turn_right")
-        m_rotate_input *= deg_to_rad(m_steering)
+func align_with_y(xform, new_y):
+	xform.basis.y = new_y
+	xform.basis.x = -xform.basis.z.cross(new_y)
+#	xform.basis = xform.basis.orthonormalized()
+	return xform.orthonormalized()
